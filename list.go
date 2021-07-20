c4c7b019ebaec351489f4e14fb7a986730fddd44
Gon <29622355+gonyyi@users.noreply.github.com>
2021-07-20 13:58:11 -0500

    Update list.go

--- list.go
+++ list.go
@@ -1,52 +1,97 @@
-package main
+package textlist
 
 import (
-	"fmt"
-	"github.com/gonyyi/textlist"
+	"bufio"
+	"bytes"
 	"os"
+	"strings"
 )
 
-func main() {
-	if len(os.Args) == 1 {
-		fmt.Printf("Usage: %s <mode> <opt>...\n", os.Args[0])
-		return
-	}
+type List map[string]struct{}
 
-	switch os.Args[1] {
-	case "compare", "cmp", "comp":
-		if len(os.Args) > 3 {
-			nochange, added, removed, err := compare(os.Args[2], os.Args[3], "::")
-			if err != nil {
-				println(err.Error())
-				return
-			}
-			for _, v := range nochange {
-				println("= " + v)
-			}
-			for _, v := range added {
-				println("+ " + v)
-			}
-			for _, v := range removed {
-				println("- " + v)
-			}
-		} else {
-			fmt.Printf("Usage: %s %s <FILE:FROM> <FILE:TO>\n", os.Args[0], os.Args[1])
-		}
-	default:
-		println("Unknown")
-	}
+func NewList(s ...string) List {
+	l := make(List)
+	l.Add(s...)
+	return l
 }
 
-func compare(fileFrom, fileTo, delim string) (nochange []string, added []string, removed []string, err error) {
-	lFrom, err := textlist.NewListFromFile(fileFrom, delim, textlist.FILE_TRIMSPACE|textlist.FILE_NEWLINE)
+func NewListFromFile(filename, delim string, flag Option) (List, error) {
+	fi, err := os.Open(filename)
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, err
 	}
-	lTo, err := textlist.NewListFromFile(fileTo, delim, textlist.FILE_TRIMSPACE|textlist.FILE_NEWLINE)
-	if err != nil {
-		return nil, nil, nil, err
+	defer fi.Close()
+
+	l := NewList()
+	bfi := bufio.NewScanner(fi)
+
+	bfi.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		if atEOF && len(data) == 0 {
+			return 0, nil, nil
+		}
+		// bytes.IndexAny will take string, but if any of the string matches, it will trigger.
+		// therefore, can't use it.
+		if i := bytes.Index(data, []byte(delim)); i >= 0 {
+			return i + len(delim), data[0:i], nil
+		}
+		// If we're at EOF, we have a final, non-terminated line. Return it.
+		if atEOF {
+			return len(data), data, nil
+		}
+		// Request more data.
+		return 0, nil, nil
+	})
+
+	for bfi.Scan() {
+		t := bfi.Text()
+		if flag&FILE_TRIMSPACE != 0 {
+			t = strings.TrimSpace(t)
+		}
+		l.Add(t)
 	}
-	added, removed = textlist.Compare(lFrom, lTo)
-	nochange = lFrom.Remove(added...).Remove(removed...).Strings()
-	return
+	return l, nil
+}
+
+func (l List) Has(s string) bool {
+	if _, ok := l[s]; ok {
+		return true
+	}
+	return false
+}
+
+func (l List) Size() int {
+	return len(l)
+}
+
+func (l List) Reset() List {
+	for v, _ := range l {
+		delete(l, v)
+	}
+	return l
+}
+
+func (l List) Add(s ...string) List {
+	for _, v := range s {
+		l[v] = struct{}{}
+	}
+	return l
+}
+
+func (l List) Remove(s ...string) List {
+	for _, v := range s {
+		delete(l, v)
+	}
+	return l
+}
+
+func (l List) String() string {
+	return strings.Join(l.Strings(), ",")
+}
+
+func (l List) Strings() []string {
+	var out []string
+	for v, _ := range l {
+		out = append(out, v)
+	}
+	return out
 }
